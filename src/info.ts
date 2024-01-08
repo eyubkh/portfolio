70f0912e81948f7ed764b6639507d33c04da7e38
Eyub <eyub@Eyubs-MacBook-Pro.local>
2024-01-08 11:18:34 +0000

    safety commit

---
+++ src/info.ts
@@ -0,0 +1,16 @@
+export const info = {
+	me: {
+		linkedin: "https://linkedin.com/in/eyubkh/",
+		github: "https://github.com/eyubkh",
+	},
+	englicus: {
+		demo: "https://youtu.be/YT3fT6dRCP0",
+		web: "https://englicus.vercel.app/",
+		repo: "https://github.com/eyubkh/Englicus",
+	},
+	lim: {
+		demo: "https://www.youtube.com/watch?v=WdJOcVGNUlM&feature=youtu.be",
+		web: "https://lim-project.vercel.app",
+		repo: "https://github.com/eyubkh/Lim",
+	},
+};
